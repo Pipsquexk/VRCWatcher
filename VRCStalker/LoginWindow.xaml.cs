1c10fd84e8baaaee6e43a2c58b5a7162a5477fba
Liam Swetnam <liamvitor@gmail.com>
2022-02-17 06:03:32 -0600

    Add project files.

---
+++ VRCStalker/LoginWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Net.Http;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using Newtonsoft.Json;
+
+namespace VRCStalker
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private bool loggedIn = false;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Login();
+        }
+
+        public void Login()
+        {
+            string userpass = $"{usernameBox.Text}:{passwordBox.Password}";
+            byte[] userpassBytes = System.Text.Encoding.UTF8.GetBytes(userpass);
+            string basicAuth = Convert.ToBase64String(userpassBytes);
+
+            CookieContainer cookies = new();
+            HttpClientHandler handler = new()
+            {
+                CookieContainer = cookies
+            };
+
+            HttpClient httpsClient = new(handler);
+            Uri reqUri = new("https://vrchat.com/api/1/auth/user?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26");
+            httpsClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.46");
+            httpsClient.DefaultRequestHeaders.Add("Authorization", $"Basic {basicAuth}");
+            HttpResponseMessage resp = httpsClient.GetAsync(reqUri).Result;
+
+            try
+            {
+                IEnumerable<Cookie> cookiess = cookies.GetCookies(reqUri).Cast<Cookie>();
+                string authCookie = cookiess.First().Value;
+
+                MainWindow.Instance.authToken = authCookie;
+
+                string username = (string)JsonConvert.DeserializeObject<dynamic>((string)resp.Content.ReadAsStringAsync().Result).username;
+
+                MainWindow.Instance.username = username;
+                MainWindow.Instance.RefreshInfo();
+                MessageBox.Show("Successfully logged into VRChat!");
+                MainWindow.Instance.Show();
+                loggedIn = true;
+                Close();
+            }
+            catch
+            {
+                MessageBox.Show("Uh oh, something went wrong.\nPlease check your credentials and try to log in again.");
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (!loggedIn) Application.Current.Shutdown();
+        }
+    }
+}
---
+++ VRCStalker/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Newtonsoft.Json;
+using System;
+using System.Windows;
+using WebSocketSharp;
+
+namespace VRCStalker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static MainWindow Instance;
+
+        public WebSocket vrcSocket;
+
+        public string username = "NULL";
+
+        public string authToken = "NULL";
+
+        private bool locationChanges = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            LoginWindow loginP = new();
+            loginP.Show();
+            Hide();
+        }
+
+        public void RefreshInfo()
+        {
+            vrcSocket = new($"wss://vrchat.com/?authToken={authToken}");
+            vrcSocket.OnOpen += VrcSocket_OnOpen;
+            vrcSocket.OnMessage += VrcSocket_OnMessage;
+            vrcSocket.Connect();
+            helloLabel.Content = $"Hello, {username}";
+        }
+
+        private void VrcSocket_OnMessage(object sender, MessageEventArgs e)
+        {
+            string jsonString = e.Data;
+            SocketInfo dyn = JsonConvert.DeserializeObject<SocketInfo>(jsonString);
+            SocketContent content = JsonConvert.DeserializeObject<SocketContent>(dyn.content);
+
+            switch (dyn.type.ToString().ToLower())
+            {
+                case "friend-location":
+
+                    if (!locationChanges) break;
+
+                    ToastContentBuilder tCB = new();
+                    tCB.AddText("Location Change");
+                    tCB.AddText($"{content.user.displayName} --> {(content.world.name == null ? "PRIVATE" : content.world.name)}");
+                    tCB.Show();
+                    break;
+            }
+
+            //type: notification, user-location, friend-location
+        }
+
+        private void VrcSocket_OnOpen(object sender, EventArgs e)
+        {
+
+        }
+
+        private void locationToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            locationChanges = true;
+        }
+
+        private void locationToggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+            locationChanges = false;
+        }
+    }
+}
